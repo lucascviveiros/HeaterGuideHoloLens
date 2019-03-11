585f1d9e61358bbc91a14cef071dfec909b29634
Lucas <lucasdicavalcanti@hotmail.com>
2019-03-11 16:10:21 +0000

    Init
    Initial commit

---
+++ HeaterGuideHoloLens/Assets/Script/AnimationCommands.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AnimationCommands : MonoBehaviour
+{
+	//Animation
+	private Animator anim;
+	private AnimatorStateInfo stateInfo;
+	//Prefabs
+	public GameObject mObj_tampa;
+	public GameObject mObj_tampa_base;
+	public GameObject mObj_adaptador;
+
+	//Animation Index Control
+	bool active = true;
+
+	//InitialPosition for "Reset"
+	Vector3 originalPosition;
+
+	void Start()
+	{
+		originalPosition = this.transform.localPosition;
+
+		mObj_tampa_base.GetComponent<Renderer>().enabled = true;
+		mObj_tampa_base.SetActive(true);
+
+		mObj_adaptador.GetComponent<Renderer>().enabled = true;
+		mObj_adaptador.SetActive(true);
+
+		anim = this.GetComponent<Animator>();
+
+	}
+
+
+	void OnSelect()
+	{
+		// If the sphere has no Rigidbody component, add one to enable physics.
+		if (!this.GetComponent<Rigidbody>())
+		{
+			var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+			rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+		}
+	}
+
+	void OnNext()
+	{
+
+		stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+		if (stateInfo.IsName("animation_1_tampa"))
+		{
+			mObj_tampa.SetActive(false);
+			mObj_tampa.GetComponent<Renderer>().enabled = false;
+
+			mObj_adaptador.SetActive(true);
+			mObj_adaptador.GetComponent<Renderer>().enabled = true;
+
+			mObj_tampa_base.SetActive(true);
+			mObj_tampa_base.GetComponent<Renderer>().enabled = true;
+			GetComponent<Animator>().Play("animation_2_tampa");
+		}
+
+		else if (stateInfo.IsName("animation_2_tampa"))
+		{
+			mObj_tampa.SetActive(false);
+			mObj_tampa.GetComponent<Renderer>().enabled = false;
+
+			mObj_tampa_base.SetActive(true);
+			mObj_tampa_base.GetComponent<Renderer>().enabled = true;
+
+			GetComponent<Animator>().Play("animation_3_tampa");
+
+		}
+		else if (stateInfo.IsName("animation_2_tampa"))
+		{
+			mObj_tampa.SetActive(false);
+			mObj_tampa.GetComponent<Renderer>().enabled = false;
+			GetComponent<Animator>().Play("animation_3_tampa");
+		}
+		else if (stateInfo.IsName("animation_3_tampa"))
+		{
+			mObj_tampa_base.SetActive(false);
+			mObj_tampa_base.GetComponent<Renderer>().enabled = false;
+
+			mObj_tampa.SetActive(false);
+			mObj_tampa.GetComponent<Renderer>().enabled = false;
+			active = false;
+		}
+	}
+
+	void OnBack()
+	{
+		stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+		if (active == false)
+		{
+			mObj_tampa.SetActive(false);
+			mObj_tampa.GetComponent<Renderer>().enabled = false;
+			mObj_tampa_base.SetActive(true);
+			mObj_tampa_base.GetComponent<Renderer>().enabled = true;
+			GetComponent<Animator>().Play("animation_3_tampa");
+			active = true;
+		}
+		else if (stateInfo.IsName("animation_3_tampa"))
+		{
+			mObj_tampa.SetActive(false);
+			mObj_tampa.GetComponent<Renderer>().enabled = false;
+
+			mObj_tampa_base.SetActive(true);
+			mObj_tampa_base.GetComponent<Renderer>().enabled = true;
+
+			GetComponent<Animator>().Play("animation_2_tampa");
+		}
+		else if (stateInfo.IsName("animation_2_tampa"))
+		{
+			mObj_tampa.SetActive(true);
+			mObj_tampa.GetComponent<Renderer>().enabled = true;
+
+			mObj_tampa_base.SetActive(true);
+			mObj_tampa_base.GetComponent<Renderer>().enabled = true;
+			GetComponent<Animator>().Play("animation_1_tampa");
+		}
+
+	}
+	
+	void OnReset()
+	{
+
+		this.transform.localPosition = originalPosition;
+	}
+
+
+	void OnExit()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
+	}
+}
+
---
+++ HeaterGuideHoloLens/Assets/Script/SpeechManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechManager : MonoBehaviour
+{
+	KeywordRecognizer keywordRecognizer = null;
+	Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+	public GameObject watertap;
+	// Use this for initialization
+	void Start()
+	{
+		keywords.Add("Reset", () =>
+		{
+			// Call the OnReset method on every descendant object.
+			this.BroadcastMessage("OnReset");
+
+		});
+
+		keywords.Add("Next", () =>
+		{
+			var focusObject = GazeGestureManager.Instance.FocusedObject;
+				focusObject.SendMessage("OnNext", SendMessageOptions.DontRequireReceiver);
+		
+		});
+
+		keywords.Add("Back", () =>
+		{
+			var focusObject = GazeGestureManager.Instance.FocusedObject;
+			focusObject.SendMessage("OnBack", SendMessageOptions.DontRequireReceiver);
+
+		});
+
+		keywords.Add("Exit", () =>
+		{
+			var focusObject = GazeGestureManager.Instance.FocusedObject;
+			focusObject.SendMessage("OnExit", SendMessageOptions.DontRequireReceiver);
+
+		});
+
+		// Tell the KeywordRecognizer about our keywords.
+		keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+		// Register a callback for the KeywordRecognizer and start recognizing!
+		keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+		keywordRecognizer.Start();
+	}
+
+	void Update()
+	{
+		// Sending commands direct to the object through mouse
+		if (Input.GetButtonDown("Fire1"))
+		{
+			watertap.SendMessage("OnNext", SendMessageOptions.DontRequireReceiver);
+
+		}
+		if (Input.GetButtonDown("Fire2"))
+		{
+			watertap.SendMessage("OnBack", SendMessageOptions.DontRequireReceiver);
+
+		}
+	}
+
+		private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+	{
+		System.Action keywordAction;
+		if (keywords.TryGetValue(args.text, out keywordAction))
+		{
+			keywordAction.Invoke();
+		}
+	}
+}
