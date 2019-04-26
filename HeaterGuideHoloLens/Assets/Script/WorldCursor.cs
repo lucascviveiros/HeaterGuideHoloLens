f5721d56689b9a0be1bdd1d5a45278b420c887b8
Lucas Cavalcanti Viveiros <lucasdicavalcanti@hotmail.com>
2019-04-26 16:11:31 +0100

    Comments fix

--- HeaterGuideHoloLens/Assets/Script/WorldCursor.cs
+++ HeaterGuideHoloLens/Assets/Script/WorldCursor.cs
@@ -4,18 +4,15 @@ public class WorldCursor : MonoBehaviour
 {
 	private MeshRenderer meshRenderer;
 
-	// Use this for initialization
 	void Start()
 	{
 		// Grab the mesh renderer that's on the same object as this script.
 		meshRenderer = this.gameObject.GetComponentInChildren<MeshRenderer>();
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
-		// Do a raycast into the world based on the user's
-		// head position and orientation.
+		// Do a raycast into the world based on the user's head position and orientation.
 		var headPosition = Camera.main.transform.position;
 		var gazeDirection = Camera.main.transform.forward;
 
@@ -23,8 +20,7 @@ public class WorldCursor : MonoBehaviour
 
 		if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
 		{
-			// If the raycast hit a hologram...
-			// Display the cursor mesh.
+			// If the raycast hit a hologram Display the cursor mesh.
 			meshRenderer.enabled = true;
 
 			// Move the cursor to the point where the raycast hit.
@@ -35,7 +31,7 @@ public class WorldCursor : MonoBehaviour
 		}
 		else
 		{
-			// If the raycast did not hit a hologram, hide the cursor mesh.
+			// If the raycast did not hit a hologram (3D Object), hide the cursor mesh.
 			meshRenderer.enabled = false;
 		}
 	}
