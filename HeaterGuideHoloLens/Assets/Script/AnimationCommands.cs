2e2be9a2a711ae2f3f9cb8c81964d55fe9f99f0b
Lucas Cavalcanti Viveiros <lucasdicavalcanti@hotmail.com>
2019-04-26 15:52:37 +0100

    Comments fix

--- HeaterGuideHoloLens/Assets/Script/AnimationCommands.cs
+++ HeaterGuideHoloLens/Assets/Script/AnimationCommands.cs
@@ -6,33 +6,29 @@ using UnityEngine.UI;
 
 public class AnimationCommands : MonoBehaviour
 {
-	// Animation
+	// Animator and animation clip info
 	private Animator anim;
 	private AnimatorStateInfo stateInfo;
 
-	//Prefabs
+	//3D Objects of the faucets
 	public GameObject mObj_tampa;
 	public GameObject mObj_tampa_base;
 	public GameObject mObj_adaptador;
 
-	// Animation Control
+	// Animation bool expression for control
 	private bool active = true;
 
-	// InitialPosition for "Reset"
+	// InitialPosition of the object for Reset command
 	Vector3 originalPosition;
 
 	void Start()
 	{
 		originalPosition = this.transform.localPosition;
-
 		mObj_tampa_base.GetComponent<Renderer>().enabled = true;
 		mObj_tampa_base.SetActive(true);
-
 		mObj_adaptador.GetComponent<Renderer>().enabled = true;
 		mObj_adaptador.SetActive(true);
-
 		anim = this.GetComponent<Animator>();
-
 	}
 
 	void OnNext()
@@ -44,40 +40,40 @@ public class AnimationCommands : MonoBehaviour
 		{
 			mObj_tampa.SetActive(false);
 			mObj_tampa.GetComponent<Renderer>().enabled = false;
-
 			mObj_adaptador.SetActive(true);
 			mObj_adaptador.GetComponent<Renderer>().enabled = true;
-
 			mObj_tampa_base.SetActive(true);
 			mObj_tampa_base.GetComponent<Renderer>().enabled = true;
 			GetComponent<Animator>().Play("animation_2_tampa");
+			
 		}
-
+		
 		else if (stateInfo.IsName("animation_2_tampa"))
 		{
 			mObj_tampa.SetActive(false);
 			mObj_tampa.GetComponent<Renderer>().enabled = false;
-
 			mObj_tampa_base.SetActive(true);
 			mObj_tampa_base.GetComponent<Renderer>().enabled = true;
-
 			GetComponent<Animator>().Play("animation_3_tampa");
-
+			
 		}
+		
 		else if (stateInfo.IsName("animation_2_tampa"))
 		{
 			mObj_tampa.SetActive(false);
 			mObj_tampa.GetComponent<Renderer>().enabled = false;
 			GetComponent<Animator>().Play("animation_3_tampa");
+			
 		}
+		
 		else if (stateInfo.IsName("animation_3_tampa"))
 		{
 			mObj_tampa_base.SetActive(false);
 			mObj_tampa_base.GetComponent<Renderer>().enabled = false;
-
 			mObj_tampa.SetActive(false);
 			mObj_tampa.GetComponent<Renderer>().enabled = false;
 			active = false;
+			
 		}
 	}
 
@@ -93,35 +89,39 @@ public class AnimationCommands : MonoBehaviour
 			mObj_tampa_base.GetComponent<Renderer>().enabled = true;
 			GetComponent<Animator>().Play("animation_3_tampa");
 			active = true;
+			
 		}
+		
 		else if (stateInfo.IsName("animation_3_tampa"))
 		{
 			mObj_tampa.SetActive(false);
 			mObj_tampa.GetComponent<Renderer>().enabled = false;
-
 			mObj_tampa_base.SetActive(true);
 			mObj_tampa_base.GetComponent<Renderer>().enabled = true;
-
 			GetComponent<Animator>().Play("animation_2_tampa");
+			
 		}
+		
 		else if (stateInfo.IsName("animation_2_tampa"))
 		{
 			mObj_tampa.SetActive(true);
 			mObj_tampa.GetComponent<Renderer>().enabled = true;
-
 			mObj_tampa_base.SetActive(true);
 			mObj_tampa_base.GetComponent<Renderer>().enabled = true;
 			GetComponent<Animator>().Play("animation_1_tampa");
+			
 		}
 
 	}
 	
+	// Methods linked to the SpeechManager class 
 	void OnReset()
 	{
 		this.transform.localPosition = originalPosition;
 	}
 
-	void OnRotateRight() {
+	void OnRotateRight() 
+	{
 		this.transform.Rotate(0, 30, 0);
 	}
 
@@ -140,11 +140,13 @@ public class AnimationCommands : MonoBehaviour
 		Time.timeScale = 0;
 	}
 
-	void OnBigger() {
+	void OnBigger() 
+	{
 		this.transform.localScale += new Vector3(0.1F, 0.1F, 0.1F);
 	}
 
-	void OnSmaller() {
+	void OnSmaller() 
+	{
 		this.transform.localScale += new Vector3(-0.1F, -0.1F, -0.1F);
 	}
 
